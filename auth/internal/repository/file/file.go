c3277b466e15489554c75bf5fed0f4f63ceb3a82
Nilson Tinassi Peres <nilsontinassiperes@MacBook-Air-de-Nilson.local>
2025-01-20 11:22:26 -0300

    Fixed token encryption and decryption, added json file storage

--- auth/internal/repository/file/file.go
+++ auth/internal/repository/file/file.go
@@ -1,23 +1,25 @@
-package file
+package main
 
 // Manage the file that stores the token
 
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"os"
 	"strings"
 	"sync"
+	"time"
 
 	encrypt "auth/internal/security"
 
-	"github.com/nilsinho42/OlistERPMediator/auth/pkg/model"
+	"auth/pkg/model"
 )
 
 type Repository struct {
 	sync.RWMutex
-	data map[string]*model.Token
+	data *model.Token
 }
 
 func (r *Repository) Get(_ context.Context) (*model.Token, error) {
@@ -25,31 +27,81 @@ func (r *Repository) Get(_ context.Context) (*model.Token, error) {
 	defer r.RUnlock()
 
 	const filename = ".token_store.json"
-	var token *model.Token
 
 	filepath, err := os.Getwd()
 	if err != nil {
-		return token, nil
+		return r.data, nil
 	}
 
 	filepath = strings.Join([]string{filepath, filename}, string(os.PathSeparator))
 	file, err := os.Open(filepath)
 	if err != nil {
-		return token, err
+		return r.data, err
 	}
 	defer file.Close()
-
 	data, err := io.ReadAll(file)
 	if err != nil {
-		return token, err
+		return r.data, err
 	}
 
-	err = json.Unmarshal(data, &token)
-
-	decrypted_token, err := encrypt.DecryptAES([]byte("key"), token.Key)
+	err = json.Unmarshal(data, &r.data)
 	if err != nil {
 		return nil, err
 	}
 
-	return decrypted_token, err
+	byte_token := []byte(r.data.Key)
+	decrypted_token, err := encrypt.DecryptAES(byte_token)
+	if err != nil {
+		return nil, err
+	}
+
+	r.data.Key = string(decrypted_token)
+	return r.data, err
+}
+
+func (r *Repository) Put(_ context.Context) error {
+	r.Lock()
+	defer r.Unlock()
+	// decrypt> 3) encrypt the token
+	encrypted_token, err := encrypt.EncryptAES([]byte(r.data.Key))
+	if err != nil {
+		return err
+	}
+
+	// decrypt> 4) update the token
+	r.data.Key = string(encrypted_token)
+
+	// decrypt> 5) update the last updated time
+	r.data.Lastupdate = time.Now().Format(time.RFC3339)
+
+	// decrypt> 6) save to the json file
+	const filename = ".token_store.json"
+	bytes, err := json.MarshalIndent(r.data, "", "   ")
+	if err != nil {
+		return err
+	}
+	os.WriteFile(filename, bytes, 0644)
+
+	return nil
+}
+
+// for testing!
+func main() {
+	r := Repository{}
+	r.data = &model.Token{}
+	r.data.Key = "34adade1-6ac4-4a5a-a394-2c47177a9311.95c5eb2f-e8a8-4f48-8bf2-fa2882f6c607.3dcda8a1-a6ef-4964-adcc-d0a5e1b8eebb"
+
+	// The TOKEN.KEY is encrypted (AES) using a key (hex-encoded) and encoded (BASE64)
+	r.Put(context.Background())
+
+	token, err := r.Get(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	bytes, err := json.MarshalIndent(token, "", " ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bytes))
 }
--- auth/internal/security/security.go
+++ auth/internal/security/security.go
@@ -4,53 +4,80 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"io"
+	"os"
 )
 
 // Security package encrypts and decrypts the token
 
-func EncryptAES(key, plaintext []byte) ([]byte, error) {
+// EncryptAES encrypts the token using a env key (hex-encoded), returns the token (string) AES encrypted
+func EncryptAES(plaintext []byte) (string, error) {
+	// encrypt> 1) get the key for encryption
+	// "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+	byte_securitykey := os.Getenv("ENCRYPTION_KEY")
+
+	// encrypt> 2) decode the key from hex-encoded
+	key, err := hex.DecodeString(byte_securitykey)
+	if err != nil {
+		return "", err
+	}
+
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	nonce := make([]byte, gcm.NonceSize()) // nonce is an arrery of bytes of length gcm.NonceSize()
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		return nil, err
+		return "", err
 	}
 
 	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil) // when creating the ciphertext, Seal appends the nonce to begin of it
 
-	return ciphertext, nil
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
 }
 
-func DecryptAES(key, ciphertext []byte) ([]byte, error) {
+func DecryptAES(ciphertext []byte) (string, error) {
+	// decrypt> 1) get the key for encryption
+	// "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
+	byte_securitykey := os.Getenv("ENCRYPTION_KEY")
+	key, err := hex.DecodeString(byte_securitykey)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext, err = base64.StdEncoding.DecodeString(string(ciphertext))
+	if err != nil {
+		return "", err
+	}
+
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	noncesize := gcm.NonceSize() // not the nonce itself, but the size of the nonce!
 	if len(ciphertext) < noncesize {
-		return nil, fmt.Errorf("invalid input")
+		return "", fmt.Errorf("invalid input")
 	}
 	nonce, cipherbytes := ciphertext[:noncesize], ciphertext[noncesize:]
 
 	plaintext, err := gcm.Open(nil, nonce, cipherbytes, nil)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	return plaintext, nil
+	return string(plaintext), nil
 }
